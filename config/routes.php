1a516b29461ee873a886828a5940488241aa4697
orken <subotm@free.fr>
2016-02-19 17:20:54 +0100

    Admin

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,22 @@
 <?php
 use Cake\Routing\Router;
 
+Router::prefix('admin', function ($routes) {
+    $routes->connect(
+        '/', [
+            'plugin' => 'Quiz',
+            'controller' => 'Users',
+            'action' => 'coucou'
+        ]
+    );
+    $routes->fallbacks('InflectedRoute');
+});
+
 Router::plugin(
     'Quiz',
     ['path' => '/quiz'],
     function ($routes) {
+    	$routes->connect('/',['controller'=>'Users','action'=>'index']);
         $routes->fallbacks('DashedRoute');
     }
 );
